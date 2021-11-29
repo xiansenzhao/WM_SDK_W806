326c09c56f4a358c737f5c68bba1958e1c09a609
xiansenzhao <zhaoxiansen52@gmail.com>
2021-11-29 20:08:58 +0800

    2021-11.29-20.08

--- tools/W806/projects/SDK_Project/project/CDK_WS/W806_SDK/Obj/xt804_cpu_task_sw.d
+++
@@ -1,5 +0,0 @@
-Obj/xt804_cpu_task_sw.o: \
- ../../../../../../../platform/component/FreeRTOS/portable/xt804/cpu_task_sw.S \
- ../../../../../../../include/arch/xt804/csi_config.h
-
-../../../../../../../include/arch/xt804/csi_config.h:
